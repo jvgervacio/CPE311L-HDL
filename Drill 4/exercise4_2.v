09933dcc0fc52552e632c9dc159c678323b554da
John Vincent P. Gervacio <jvgervacio0012@gmail.com>
2023-02-20 18:48:41 +0800

    added exercise4_1  and 2

---
+++ Drill 4/exercise4_2.v
@@ -0,0 +1,55 @@
+primitive binary_subtractor(D, A, B, Cin);
+	input A, B, Cin;
+	output D;
+	table
+//  A B Cin D
+	0 0 0 : 0;
+	0 0 1 : 1;
+	0 1 0 : 1;
+	0 1 1 : 0;
+	1 0 0 : 1;
+	1 0 1 : 0;
+	1 1 0 : 0;
+	1 1 1 : 1;
+	endtable
+endprimitive
+
+primitive Carry_out (Cout, A, B, Cin);
+	input A, B, Cin;
+	output Cout;
+	table
+//	A B Cin Cout
+	0 0 0 : 0;
+	0 0 1 : 1;
+	0 1 0 : 1;
+	0 1 1 : 1;
+	1 0 0 : 0;
+	1 0 1 : 0;
+	1 1 0 : 0;
+	1 1 1 : 1;
+	endtable
+endprimitive
+
+module exercise4_2;
+	reg A, B, Cin;
+	wire [1:0] D;
+	
+	binary_subtractor   (D[0], A, B, Cin);
+	Carry_out           (D[1], A, B, Cin);
+	
+	initial begin
+		$display("Binary Subtrator");
+		$display("A\tB\tCin\t:\tDiff\tCout");
+		$monitor("%b\t%b\t%b\t:\t%b\t%b", A, B, Cin, D[0], D[1]);
+		#1 A=1'bx;      B=1'bx;     Cin=1'bx;
+		#1 A=1'b0;      B=1'b0;     Cin=1'b0;
+		#1 A=1'b0;      B=1'b0;     Cin=1'b1;
+		#1 A=1'b0;      B=1'b1;     Cin=1'b0;
+		#1 A=1'b0;      B=1'b1;     Cin=1'b1;
+		#1 A=1'b1;      B=1'b0;     Cin=1'b0;
+		#1 A=1'b1;      B=1'b0;     Cin=1'b1;
+		#1 A=1'b1;      B=1'b1;     Cin=1'b0;
+		#1 A=1'b1;      B=1'b1;     Cin=1'b1;
+		#1 $finish;
+	end
+endmodule
